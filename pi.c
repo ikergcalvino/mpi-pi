7fabd95bb70e7b5b50e3855243fbb61e10943d3e
Íker García Calviño <iker.gcalvino@udc.es>
2021-04-14 19:08:37 +0200

    p1 first version

--- pi.c
+++ pi.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <mpi.h>
 
 int main(int argc, char *argv[])
 {
@@ -7,21 +8,53 @@ int main(int argc, char *argv[])
     double PI25DT = 3.141592653589793238462643;
     double pi, h, sum, x;
 
+    int k;
+    int numprocs, rank, recbuff;
+
+    MPI_Status status;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
     while (!done)
     {
-        printf("Enter the number of intervals: (0 quits) \n");
-        scanf("%d",&n);
+        if (rank == 0)
+        {
+            printf("Enter the number of intervals: (0 quits) \n");
+            scanf("%d",&n);
+            for (k = 1; k < numprocs; k++)
+                MPI_Send(&n, 1, MPI_INT, k, 0, MPI_COMM_WORLD);
+        } else
+        {
+            MPI_Recv(&n, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        }
     
         if (n == 0) break;
   
-        h   = 1.0 / (double) n;
-        sum = 0.0;
-        for (i = 1; i <= n; i++) {
-            x = h * ((double)i - 0.5);
-            sum += 4.0 / (1.0 + x*x);
+        for (i = rank + 2; i < n; i += numprocs)
+        {
+            h   = 1.0 / (double) n;
+            sum = 0.0;
+            for (i = 1; i <= n; i++) {
+                x = h * ((double)i - 0.5);
+                sum += 4.0 / (1.0 + x*x);
+            }
+            pi = h * sum;
         }
-        pi = h * sum;
 
-        printf("pi is approximately %.16f, Error is %.16f\n", pi, fabs(pi - PI25DT));
+        if (rank == 0)
+        {
+            for (k = 1; k < numprocs; k++)
+            {
+                MPI_Recv(&recbuff, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+                pi += recbuff;
+            }
+            printf("pi is approximately %.16f, Error is %.16f\n", pi, fabs(pi - PI25DT));
+        } else
+        {
+            MPI_Send(&pi, 1, MPI_INT, 0, rank, MPI_COMM_WORLD);
+        }
     }
+    MPI_Finalize();
 }
