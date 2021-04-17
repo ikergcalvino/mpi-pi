ab7a880371bed00c6c0c9ca16a1d6296623b8aee
Íker García Calviño <iker.gcalvino@udc.es>
2021-04-17 20:42:19 +0200

    pi bug fixed and loop upgraded

--- pi.c
+++ pi.c
@@ -9,7 +9,8 @@ int main(int argc, char *argv[])
     double pi, h, sum, x;
 
     int k;
-    int numprocs, rank, pirec;
+    double pirec;
+    int numprocs, rank;
 
     MPI_Status status;
 
@@ -32,25 +33,22 @@ int main(int argc, char *argv[])
     
         if (n == 0) break;
   
-        for (i = rank + 2; i < n; i += numprocs)
-        {
-            h   = 1.0 / (double) n;
-            sum = 0.0;
-            for (i = 1; i <= n; i++) {
-                x = h * ((double)i - 0.5);
-                sum += 4.0 / (1.0 + x*x);
-            }
-            pi = h * sum;
+        h   = 1.0 / (double) n;
+        sum = 0.0;
+        for (i = rank; i <= n; i += numprocs) {
+            x = h * ((double)i - 0.5);
+            sum += 4.0 / (1.0 + x*x);
         }
+        pi = h * sum;
 
         if (rank > 0)
         {
-            MPI_Send(&pi, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+            MPI_Send(&pi, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
         } else
         {
             for (k = 1; k < numprocs; k++)
             {
-                MPI_Recv(&pirec, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+                MPI_Recv(&pirec, 1, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
                 pi += pirec;
             }
             printf("pi is approximately %.16f, Error is %.16f\n", pi, fabs(pi - PI25DT));
